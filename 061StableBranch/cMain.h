e0647d6e684e1d1f4eac4ee27babab86170ced85
doctashay <shabennett2@gmail.com>
2020-11-20 18:47:18 -0700

    Repurpose project for Borderlands 3

--- 061StableBranch/cMain.h
+++ 061StableBranch/cMain.h
@@ -9,16 +9,3 @@
 
 using namespace std;
 
-#define VK_W 0x57
-#define VK_S 0x53
-
-extern bool ExplosiveFists;
-extern bool GrenadeFists;
-extern bool MosinFists;
-extern bool speedhack;
-extern bool playerESP;
-extern bool ESP;
-extern bool vehicleESP;
-extern bool noGrass;
-extern int MainWindow();
-
--- 061StableBranch/hMain.h
+++ 061StableBranch/hMain.h
@@ -1,20 +1,9 @@
 #ifndef HMAIN_H
 #define HMAIN_H
 
-#include "hDirectX.h"
 
 #include <Windows.h>
 #include <iostream>
-
-#include <d3d9.h>
-#include <d3dx9.h>
-#pragma comment(lib, "d3d9.lib")
-#pragma comment(lib, "d3dx9.lib")
-
-#include <dwmapi.h>
-#pragma comment(lib, "dwmapi.lib")
-extern HWND tWnd;
 extern HANDLE hProcess;
-int DrawText(char* String, int x, int y, DWORD color);
 
 #endif
